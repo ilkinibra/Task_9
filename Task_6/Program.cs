2addcda390fc68841ab28b58c5492100b7a0c79a
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2022-04-24 07:42:18 +0400

    Add files via upload

---
+++ Task_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Programming");
+            student.Fullname = "ilkin ibrahimov";
+
+            Student student1 = new Student("Programming");
+            student1.Fullname = "tural memmedzade";
+
+            Student student2 = new Student("Design");
+            student2.Fullname = "orxan qenberov";
+            Console.WriteLine(student.Groupname);
+            Console.WriteLine(student1.Groupname);
+            Console.WriteLine(student2.Groupname);
+
+
+        }
+    }
+}
---
+++ Task_6/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Task_6
+{
+    public class Student
+    {
+        private static int _pId = 1000;
+        private static int _dId = 1000;
+
+        private static int _id;
+        public int Id { get; }
+        public string Fullname { get; set; }
+        public string Subjectname { get; set; }
+        private string _groupname;
+        public string Groupname
+        {
+            get
+            {
+                return _groupname;
+            }
+        }
+
+        public Student(string subjectName)
+        {
+            _id++;
+            Id = _id;
+            Subjectname = subjectName;
+            if (subjectName == "Programming")
+            {
+                _pId++;
+                _groupname = Subjectname.Substring(0, 2).ToUpper() + _pId;
+            }
+            else if (subjectName == "Design")
+            {
+                _dId++;
+                _groupname = Subjectname.Substring(0, 2).ToUpper() + _dId;
+            }
+        }
+    }
+}
